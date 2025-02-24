544acb0e31406bba9abb931889cd2d031613d711
Cxnnon <Csumirya@gmail.com>
2025-02-24 22:12:58 +0700

    Update starter.js

--- setup/starter.js
+++ setup/starter.js
@@ -1,7 +1,7 @@
 module.exports = {
     tk: [
-        "ODc0ODk4NDIyMjMzMTc4MTQy.",   // Token 1
-        "MTE5MTM4NDQ3MzM4MDU4OTYwOA"   // Token 2 (Can be deleted if not needed)
+        "OTEwODA0NDY3NzIzNjczNjMw.GVIyMY.HaVvQ10t57XqD9sQdV9qfanD-nil4YA5Rd7yAY",   // Token 1
+        
     ],
 
     config: require("./config.json")
